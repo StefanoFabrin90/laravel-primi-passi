9ad14b60ead8f5cdc79812e024fa07beb4f9d60a
stef_fab@hotmail.it <stef_fab@hotmail.it>
2022-01-18 14:44:50 +0100

    add 1 primi passi laravel

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    //return view('welcome');
+    return view('home', [
+        'title' => 'Hello World 45!',
+        'text' => 'lorem ipsum dolor sit amet'
+    ]);
+})->name('homepage');
+
+Route::get('/about-us', function () {
+    return view('about');
+})->name('about');
